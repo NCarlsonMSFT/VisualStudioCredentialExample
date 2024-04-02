43ffc6bdc899c60aa8ca588f239838f9412685a7
Nathan Carlson <ncarlson@microsoft.com>
2024-04-02 16:49:52 -0700

    Add an AOT example project (not working yet)

---
+++ VisualStudioCredentialAOTExample/Program.cs
@@ -0,0 +1,7 @@
+﻿using Azure.Identity;
+using Azure.ResourceManager;
+
+ArmClient client = new ArmClient(new VisualStudioCredential());
+var subscriptionResource = await client.GetDefaultSubscriptionAsync();
+var subscription = await subscriptionResource.GetAsync();
+Console.WriteLine($"DisplayName: {subscription.Value.Data.DisplayName}");
